6f0e228779189335a76c7fc843259abd7af0ed63
Rome Bell <romebell5@gmail.com>
2021-05-13 13:33:05 -0700

    auth: add signup post route

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const passport = require('../config/ppConfig');
 
+const db = require('../models');
+
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
 });
@@ -16,5 +18,36 @@ router.post('/login', passport.authenticate('local', {
   successFlash: 'Welcome back ...',
   failureFlash: 'Either email or password is incorrect. Please try again.'
 }));
+
+router.post('/signup', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    const [user, created] = await db.user.findOrCreate({
+      where: { email },
+      defaults: { name, password }
+    });
+
+    if (created) {
+      console.log(`------ ${user.name} was created ------`);
+      const successObject = {
+        successRedirect: '/',
+        successFlash: `Welcome ${user.name}. Account was created.`
+      }
+
+      passport.authenticate('local', successObject)(req, res);
+    } else {
+      req.flash('error', 'Email already exists');
+      res.redirect('/auth/signup');
+    }
+
+  } catch (error) {
+    console.log('------ Error --------');
+    console.log(error);
+    // Handle the user in case something goes wrong
+    req.flash('error', 'Either email or password is incorrect. Please try again.');
+    res.redirect('/auth/signup');
+  }
+});
 
 module.exports = router;
